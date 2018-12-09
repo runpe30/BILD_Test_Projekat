63dd9b8631a50576ce0c85f13b65a449d2b358f1
runpe30 <34744490+runpe30@users.noreply.github.com>
2018-12-09 20:32:23 +0100

    Add files via upload

---
+++ DevicesApp/DevicesApp/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace DevicesApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Unesite uredjaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ime uredjaja";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ID";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(261, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(527, 301);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(353, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Prikazi uredjaj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(582, 322);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(507, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "ID ure]aja";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(353, 351);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Brisanje";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(527, 354);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(155, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Izbrisite trenutni uredjaj";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 197);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 155);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(13, 127);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
---
+++ DevicesApp/DevicesApp/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DevicesApp
+{
+    public partial class Form1 : Form
+    {
+
+        String connection = @"Server=DESKTOP-1HFCEM8\SQLExpress2; Database=DevicesDB; Integrated Security=true";
+        SqlConnection Conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Conn = new SqlConnection();
+                Conn.ConnectionString = connection;
+                Conn.Open();
+                //MessageBox.Show("Database is ready.");
+            }
+
+            catch (Exception err)
+            {
+                MessageBox.Show("Error!");
+            }
+
+            Ucitaj();
+            
+            
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            //String upit = "SELECT ([Name], [Parent]) FROM DeviceTypes";
+            //SqlCommand komanda = new SqlCommand();
+            //try
+            //{
+            //    komanda.CommandText = upit;
+            //    komanda.Connection = Conn;
+            //    SqlDataAdapter adapter = new SqlDataAdapter();
+            //    adapter.SelectCommand = komanda;
+                
+                
+            //}
+            //catch (Exception err)
+            //{
+            //    MessageBox.Show("neeee");
+            //}
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idParent = Int32.Parse(textBox1.Text);
+            String ime = textBox2.Text;
+            String upit = "INSERT INTO DeviceTypes VALUES(@imeParam, @idParentParam)";
+            SqlCommand komanda = new SqlCommand();
+            try
+            {
+                komanda.CommandText = upit;
+                komanda.Connection = Conn;
+                komanda.Parameters.Add("idParentParam", SqlDbType.Int);
+                komanda.Parameters.Add("imeParam", SqlDbType.NVarChar);
+                komanda.Parameters["idParentParam"].Value = idParent;
+                komanda.Parameters["imeParam"].Value = ime;
+                komanda.ExecuteNonQuery();
+                MessageBox.Show("uspjesno upisano");
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error " + err);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            int idP = Int32.Parse(textBox3.Text);
+            DataTable table = new DataTable();
+            String upit = "SELECT id, Name, parentID FROM DeviceTypes where ID=@Param";
+            SqlCommand komanda = new SqlCommand();
+            try
+            {
+                komanda.CommandText = upit;
+                komanda.Connection = Conn;
+                komanda.Parameters.Add("Param", SqlDbType.Int);
+                komanda.Parameters["Param"].Value = idP;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = komanda;
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String upit = "DELETE FROM DeviceTypes WHERE ID=@idParam";
+            if (dataGridView1.SelectedRows.Count < 1)
+            {
+                MessageBox.Show("izaberite red");
+                return;
+            }
+            int id = Int32.Parse(dataGridView1.SelectedRows[0].Cells["ID"].Value.ToString());
+            SqlCommand komanda = new SqlCommand();
+            try
+            {
+                komanda.CommandText = upit;
+                komanda.Connection = Conn;
+                komanda.Parameters.Add("idParam", SqlDbType.Int);
+                komanda.Parameters["idParam"].Value = id;
+                komanda.ExecuteNonQuery();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error!");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+            
+            //String upit = "SELECT * FROM DeviceTypes";
+            //SqlCommand komanda = new SqlCommand();
+            //try
+            //{
+            //    komanda.CommandText = upit;
+            //    komanda.Connection = Conn;
+            //    SqlDataReader reader = komanda.ExecuteReader();
+            //    while (reader.Read())
+            //    {
+            //        comboBox1.Items.Add(reader["Name"].ToString());
+            //    }
+            //    reader.Close();
+
+
+            //}
+            //catch (Exception err)
+            //{
+            //    MessageBox.Show("neeee");
+            //}
+        }
+
+        public void Ucitaj()
+        {
+            String upit = "SELECT * FROM DeviceTypes";
+            SqlCommand komanda = new SqlCommand();
+            try
+            {
+                komanda.CommandText = upit;
+                komanda.Connection = Conn;
+                SqlDataReader reader = komanda.ExecuteReader();
+                while (reader.Read())
+                {
+                    comboBox1.Items.Add(reader["Name"].ToString());
+                }
+                reader.Close();
+
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("neeee");
+            }
+        }
+
+        public void SelectID ()
+        {
+            String str;
+            String upit = "SELECT * FROM DeviceTypes WHERE Name=str";
+            SqlCommand komanda = new SqlCommand();
+            try
+            {
+                str = comboBox1.SelectedItem.ToString();
+                komanda.CommandText = upit;
+                komanda.Connection = Conn;
+
+                SqlDataReader reader = komanda.ExecuteReader();
+                while (reader.Read())
+                {
+                    textBox1.Text = (reader["ID"].ToString());
+                }
+                reader.Close();
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error!");
+            }
+        }
+
+    }
+}
